21412c0b0c763af5d5c0b57ec43971a8b436e885
Hank <hcheng10@gmail.com>
2015-08-09 02:47:12 -0700

    initial commit

---
+++ plot1.R
@@ -0,0 +1,9 @@
+## read in data
+data <- read.table("./household_power_consumption.txt", header = TRUE, sep=";")
+
+## subset out data
+data <- data[data$Date == "1/2/2007" | data$Date == "2/2/2007",]
+
+##make histogram of global active power, must convert from factor to numeric
+grp <- as.numeric(as.character(data$Global_active_power))
+hist(grp, main = "Global Active Power", col="red", xlab = "Global Active Power (kilowatts)")
