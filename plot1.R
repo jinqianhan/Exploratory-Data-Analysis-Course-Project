516f74aba4c29e131a42c12b02193e79def05895
Hank <hcheng10@gmail.com>
2015-08-09 06:16:27 -0700

    fixed error in plot 2

--- plot1.R
+++ plot1.R
@@ -7,3 +7,5 @@ data <- data[data$Date == "1/2/2007" | data$Date == "2/2/2007",]
 ##make histogram of global active power, must convert from factor to numeric
 grp <- as.numeric(as.character(data$Global_active_power))
 hist(grp, main = "Global Active Power", col="red", xlab = "Global Active Power (kilowatts)")
+dev.copy(png, file = "plot1.png")
+dev.off()
