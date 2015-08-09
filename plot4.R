1f718a804245188fb47fc09aea376512246a7d6f
Hank <hcheng10@gmail.com>
2015-08-09 07:02:56 -0700

    finished plot 4, fixed legend issue in plot 3

--- plot4.R
+++ plot4.R
@@ -30,16 +30,28 @@ dat <- cbind(data,tc)
 dat<- mutate(dat, sub1 = as.numeric(as.character(dat$Sub_metering_1)))
 dat<- mutate(dat, sub2 = as.numeric(as.character(dat$Sub_metering_2)))
 dat<- mutate(dat, sub3 = as.numeric(as.character(dat$Sub_metering_3)))
+dat<- mutate(dat, vol = as.numeric(as.character(dat$Voltage)))
+dat <- mutate(dat, reactpow = as.numeric(as.character(dat$Global_reactive_power)))
 
 ##open the file device
-png(file="plot3.png")
+png(file="plot4.png")
 
-##plot initial graphs
+## prepare graph environment
+par(mfrow = c(2,2))
+
+## graph first graph
+dat <- mutate(dat, kwatt= as.numeric(as.character(dat$Global_active_power)))
+plot(dat$tc, dat$kwatt, type = "l", xlab = "", ylab = "Global Active Power")
+## graph second graph
+plot(dat$tc, dat$vol, type="l", xlab = "datetime", ylab = "Voltage")
+## plot graph 3
 plot(dat$tc, dat$sub1, type="l", col="black", ylab = "Energy sub metering", xlab="")
 lines(dat$tc, dat$sub2, type="l", col="red")
 lines(dat$tc, dat$sub3, type="l", col="blue")
-##add legend
-legend("topright", lty=1, col = c("black", "red", "blue"), legend= c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", lty=1, col = c("black", "red", "blue"), legend= c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty = "n")
+## plot final graph
+plot(dat$tc, dat$reactpow, type="l", xlab = "datetime", ylab = "Global_reactive_power")
 
-##close device
+##convert to png
+##dev.copy(png, file = "plot4.png")
 dev.off()
